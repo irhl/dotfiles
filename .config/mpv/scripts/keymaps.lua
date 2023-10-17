10838d3a3fd70dbe87f523fd05972417344fc756
mako <uwu@sunflowering.pw>
2023-10-17 22:44:00 +0000

    reassign keybindings

--- .config/mpv/scripts/keymaps.lua
+++ .config/mpv/scripts/keymaps.lua
@@ -1,50 +1,47 @@
-local mp = require 'mp'
-
 local key = {
-    ["q"] = "quit",
-    ["m"] = "cycle mute",
-    ["0"] = "add volume 15",
-    ["9"] = "add volume -15",
-    ["a"] = "playlist-next",
-    ["z"] = "playlist-prev",
-    ["left"]  = "seek -10",
-    ["right"] = "seek 10",
-    ["space"] = "cycle pause",
+  q = "quit",
+  z = "playlist-next",
+  Z = "playlist-prev",
+  x = "add volume 15",
+  X = "add volume -15",
+  m = "cycle mute",
+  left  = "seek -10",
+  right = "seek 10",
+  space = "cycle pause",
 
-    ["h"] = "cycle",
-    ["H"] = "cycle sub down",
-    ["v"] = "cycle sub visibility",
+  c = "cycle sub",
+  C = "cycle sub down",
+  v = "cycle sub visibility",
 
-    -- Pitch shift
-    ["f2"] = "af toggle asetrate=44100*1.08",
-    ["f3"] = "af toggle asetrate=44100*1.12",
-    ["f4"] = "af toggle asetrate=44100*1.",
-    ["f5"] = "af toggle asetrate=44100*1.32",
-    ["f6"] = "af toggle asetrate=44100*1.52",
+  h = "add video-pan-x -0.1",
+  j = "add video-pan-y -0.1",
+  k = "add video-pan-y 0.1",
+  l = "add video-pan-x 0.1",
+  f = "add video-zoom 0.1",
+  F = "add video-zoom -0.1",
+  r = "set video-zoom 0 ; " ..
+      "set video-pan-x 0 ; " ..
+      "set video-pan-y 0",
 
-    -- Bass Boost
-    ["f1"] = "af toggle superequalizer=2b=3:3b=1:4b=2",
+  e = "cycle_values video-rotate 90 180 270 0",
 
-    -- Window Manipulation
-    ["="] = "add video-zoom 0.1",
-    ["-"] = "add video-zoom -0.1",
-    ["alt+up"] = "add video-pan-y 0.1",
-    ["alt+down"] = "add video-pan-y -0.1",
-    ["alt+left"] = "add video-pan-x 0.1",
-    ["alt+right"] = "add video-pan-x -0.1",
-    ["e"] = "cycle_values video-rotate 90 180 270 0",
-    ["r"] = "set video-zoom 0 ; set video-pan-x 0 ; set video-pan-y 0",
+  f1 = "af toggle superequalizer=2b=3:3b=1:4b=2",
+  f2 = "af toggle asetrate=44100*1.08",
+  f3 = "af toggle asetrate=44100*1.12",
+  f4 = "af toggle asetrate=44100*1.22",
+  f5 = "af toggle asetrate=44100*1.32",
+  f6 = "af toggle asetrate=44100*1.52",
 }
 
 local reply = function()
-    for key, command in pairs(key) do
+    for key, action in pairs(key) do
         local options = {}
         if key == "left" or key == "right" then
             options = { "repeatable" }
         end
 
         mp.add_forced_key_binding(key, function()
-            mp.command(command)
+            mp.command(action)
         end, unpack(options))
     end
 end
